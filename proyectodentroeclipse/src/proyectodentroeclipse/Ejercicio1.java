c37eb94d1e7a7326df32b1a012c187dc04b9e228
David Carrasco <yendaadan@gmail.com>
2022-06-17 20:00:14 +0200

    Archivos con errores

---
+++ proyectodentroeclipse/src/proyectodentroeclipse/Ejercicio1.java
@@ -0,0 +1,9 @@
+package proyectodentroeclipse;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("hola")
+	}
+
+}
---
+++ proyectodentroeclipse/src/proyectodentroeclipse/Ejercicio2.java
@@ -0,0 +1,10 @@
+package proyectodentroeclipse;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		System.out.println("hola);
+
+	}
+
+}
